0eb65d6253ee5d7255ecc847027554d2ffd4bf49
Alastair Houghton <alastair@alastairs-place.net>
2014-12-04 17:38:40 +0000

    Explicitly use floor division where necessary.  Also, fix an incorrect tag.

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='mac_alias',
+      version='1.0.0',
+      description='Generate/parse Mac OS Alias records from Python',
+      long_description=u"""
+``mac_alias`` lets you generate or read binary Alias records from Python code.
+While it is written in pure Python, some OS X specific code is required to
+generate a proper Alias record for a given file, so this module currently
+is not portable to other platforms.
+
+Credit is due to Wim Lewis <wiml@hhhh.org>, whose work on reverse engineering
+the alias format was extremely useful in writing this code.""",
+      author='Alastair Houghton',
+      author_email='alastair@alastairs-place.net',
+      url='http://alastairs-place.net/projects/mac_alias',
+      license='MIT License',
+      platforms='darwin',
+      packages=['mac_alias'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Desktop Environment',
+          'Topic :: Software Development :: Libraries :: Python Modules'],
+      provides=['mac_alias'])
+
