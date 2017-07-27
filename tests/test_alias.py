4672a3f95121cb669b90617516aba6dc9badc571
Alastair Houghton <alastair@alastairs-place.net>
2017-07-27 14:26:20 +0100

    Added a couple of very basic tests.

---
+++ tests/test_alias.py
@@ -0,0 +1,6 @@
+from mac_alias import Alias
+
+def test_simple_alias():
+    a = Alias.for_file('/Applications')
+
+    assert a is not None
---
+++ tests/test_bookmark.py
@@ -0,0 +1,6 @@
+from mac_alias import Bookmark
+
+def test_simple_bookmark():
+    b = Bookmark.for_file('/Applications')
+
+    assert b is not None
