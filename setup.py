8f86e2b53e43981d963e26e8b1d81821b95f19c7
Alastair Houghton <alastair@alastairs-place.net>
2017-07-27 15:20:20 +0100

    Bumped version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.rst', 'rb') as f:
     longdesc = f.read().decode('utf-8')
 
 setup(name='mac_alias',
-      version='2.0.1',
+      version='2.0.2',
       description='Generate/parse Mac OS Alias records from Python',
       long_description=longdesc,
       author='Alastair Houghton',
